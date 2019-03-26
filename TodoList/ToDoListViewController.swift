1cfc6f78a99c8e44a6a6a816ffb42f8b2417ba44
Aleksandra Marzec <olamarzec2@vp.pl>
2019-03-26 21:33:35 +0100

    First commit with dataSource and delegate method

---
+++ TodoList/ToDoListViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by AIR on 26.03.2019.
+//  Copyright © 2019 AIR. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    
+    let itemArray = ["buy egg", "chose a milk", "change on coconut water", "Filtr a water"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      //  print(itemArray[indexPath.row])
+    
+        
+        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
